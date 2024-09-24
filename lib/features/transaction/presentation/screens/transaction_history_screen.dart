22be5c54b48346bdbb3bbf75d62c785e0e1f0fb3
Lavyne Owiti <owitilavyne@gmail.com>
2024-09-24 19:41:27 +0300

    feat: new add

---
+++ lib/features/transaction/presentation/screens/transaction_history_screen.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/cupertino.dart';
+
+class HistoricalTransactionScreen extends StatelessWidget {
+  const HistoricalTransactionScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
---
+++ lib/features/transaction/presentation/widgets/transaction_form.dart
@@ -0,0 +1,17 @@
+
+import 'package:flutter/material.dart';
+
+class TransactionForm extends StatefulWidget {
+  const TransactionForm({super.key});
+
+  @override
+  State<TransactionForm> createState() => _TransactionFormState();
+}
+
+class _TransactionFormState extends State<TransactionForm> {
+  @override
+  Widget build(BuildContext context) {
+    return const Form(child: child);
+    
+  }
+}
